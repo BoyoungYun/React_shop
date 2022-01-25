775caa5f860f39da9ce9ef5436aa6e5ae06199f8
Boyoung Yun <index_1234@naver.com>
2022-01-25 22:45:39 +0900

    메인 화면 제작 (using Bootstrap) - 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">ShoesShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className="background">
+        <h1 className="display-4"><b>20% Sales Off</b></h1>
+        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr className="my-4"></hr>
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p className="lead">
+          <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
+      <div className="container">
+      <div className="row">
+        <div className="col-md-4">
+          <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+          <h4>상품명</h4>
+          <p>상품설명 & 가격</p>
+        </div>
+        <div className="col-md-4">
+          <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%" />
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+        </div>
+        <div className="col-md-4">
+          <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%" />
+            <h4>상품명</h4>
+            <p>상품설명 & 가격</p>
+        </div>
+      </div>
     </div>
+
+    </div>
+
+    
   );
 }
 
 export default App;
+/* 출처 : 코딩애플 - React 리액트 기초부터 쇼핑몰 프로젝트까지! */
