4996b8737ecbb1deb1f6024418d8ff144c07f2e7
Boyoung Yun <index_1234@naver.com>
2022-02-02 15:59:42 +0900

    상세페이지 제작(styled-components, scss 사용)

--- src/Detail.js
+++ src/Detail.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import Data from './data';
+import styled from 'styled-components';
+import './Detail.scss';
 
+let 박스 = styled.div`
+  padding : 20px;
+`;
+let 제목 = styled.h4`
+  font-size : 25px;
+`;
 function Detail(props)
 {
-  
   let { id } = useParams();
   let navigate = useNavigate();
   return (
     <div className="container">
+      <박스><제목 className="red">Detail</제목></박스>
+      <div className="my-alert-yellow">
+        <p>재고가 얼마 남지 않았습니다</p>
+      </div>
       <div className="row">
         <div className="col-md-6">
           <img src={"https://codingapple1.github.io/shop/shoes"+(Number(id)+1)+".jpg"} width="100%" />
